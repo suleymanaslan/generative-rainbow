4e8aa9f880b7e2e255bd7daaa7e117f7bd267fdb
suleymanaslan <suleymanaslan15@gmail.com>
2020-09-06 20:08:31 +0300

    Move PGAN to agent

--- agent.py
+++ agent.py
@@ -1,11 +1,16 @@
 # adapted from https://github.com/Kaixhin/Rainbow
 
-import numpy as np
+import time
 import torch
+import imageio
+import numpy as np
 import torch.optim as optim
 from torch.nn.utils import clip_grad_norm_
 
-from network import DQN
+from network import DQN, GeneratorDQN, PGANDiscriminator
+from pytorch_GAN_zoo.models.loss_criterions import base_loss_criterions
+from pytorch_GAN_zoo.models.loss_criterions.gradient_losses import WGANGPGradientPenalty
+from pytorch_GAN_zoo.models.utils.utils import finiteCheck
 
 
 class Agent:
@@ -26,7 +31,13 @@ class Agent:
         self.norm_clip = norm_clip
         self.noisy_std = noisy_std
 
-        self.online_net, self.target_net = self._get_nets()
+        self.online_net, self.target_net, self.discrm_net = self._get_nets()
+
+        for _ in range(5):
+            self.discrm_net.add_scale(depth_new_scale=128)
+            self.online_net.add_scale(depth_new_scale=128)
+        self.discrm_net.to(self.device)
+        self.online_net.to(self.device)
 
         self.online_net.train()
         self.update_target_net()
@@ -35,17 +46,31 @@ class Agent:
         for param in self.target_net.parameters():
             param.requires_grad = False
 
-        self.optimizer = optim.Adam(self.online_net.parameters(), lr=lr, eps=adam_eps)
+        self.optimizer_o = optim.Adam(filter(lambda p: p.requires_grad, self.online_net.parameters()),
+                                      betas=(0, 0.99), lr=lr, eps=adam_eps)
+        self.optimizer_d = optim.Adam(filter(lambda p: p.requires_grad, self.discrm_net.parameters()),
+                                      betas=(0, 0.99), lr=lr)
+
+        self.optimizer_o.zero_grad()
+        self.optimizer_d.zero_grad()
+
+        self.loss_criterion = base_loss_criterions.WGANGP(self.device)
+        self.epsilon_d = 0.001
+
+        self.real_state = None
+        self.real_next_state = None
+        self.generated_state = None
 
         if load_file:
             self.load(f"trained_models/{load_file}")
 
     def _get_nets(self):
-        online_net = DQN(self.atoms, self.action_size, self.env.window, self.hidden_size, self.noisy_std) \
+        online_net = GeneratorDQN(self.atoms, self.action_size, self.env.window, self.hidden_size, self.noisy_std) \
             .to(self.device)
         target_net = DQN(self.atoms, self.action_size, self.env.window, self.hidden_size, self.noisy_std) \
             .to(self.device)
-        return online_net, target_net
+        discrm_net = PGANDiscriminator(self.env.window)
+        return online_net, target_net, discrm_net
 
     def train(self):
         self.online_net.train()
@@ -61,15 +86,28 @@ class Agent:
         self.online_net.load_state_dict(torch.load(f"{load_dir}/online_net.pth"))
         self.target_net.load_state_dict(torch.load(f"{load_dir}/target_net.pth"))
 
+    def save_generated(self, save_dir):
+        real_state = (self.real_state.numpy() * 255).astype(np.uint8)
+        real_next_state = (self.real_next_state.numpy() * 255).astype(np.uint8)
+        generated_state = self.generated_state.numpy()
+        generated_state = ((generated_state - generated_state.min()) / (
+                generated_state.max() - generated_state.min()) * 255).astype(np.uint8)
+        pgan_img = np.concatenate((real_state, real_next_state, generated_state), axis=1)
+        imageio.imwrite(f"{save_dir}/{int(time.time())}.jpg", pgan_img)
+
     def update_target_net(self):
-        self.target_net.load_state_dict(self.online_net.state_dict())
+        self.target_net.net.load_state_dict(self.online_net.net.state_dict())
+        self.target_net.fc_h_v.load_state_dict(self.online_net.fc_h_v.state_dict())
+        self.target_net.fc_h_a.load_state_dict(self.online_net.fc_h_a.state_dict())
+        self.target_net.fc_z_v.load_state_dict(self.online_net.fc_z_v.state_dict())
+        self.target_net.fc_z_a.load_state_dict(self.online_net.fc_z_a.state_dict())
 
     def reset_noise(self):
         self.online_net.reset_noise()
 
     def _act(self, state):
         with torch.no_grad():
-            q, x = self.online_net(state.unsqueeze(0))
+            q, x = self.online_net(state.unsqueeze(0), skip_gan=True)
             return (q * self.support).sum(2).argmax(1).item(), x
 
     def act(self, state):
@@ -82,15 +120,12 @@ class Agent:
         else:
             return action, features
 
-    def _learn(self, mem, pgan, train_agent, idxs, states, actions, returns, next_states, nonterminals, weights):
-        log_ps, features = self.online_net(states, use_log_softmax=True)
-        pgan.learn(states, features, actions, next_states)
-        if not train_agent:
-            return
+    def _learn(self, mem, idxs, states, actions, returns, next_states, nonterminals, weights):
+        log_ps, features = self.online_net(states, skip_gan=True, use_log_softmax=True)
         log_ps_a = log_ps[range(self.batch_size), actions]
 
         with torch.no_grad():
-            pns, _ = self.online_net(next_states)
+            pns, _ = self.online_net(next_states, skip_gan=True)
             dns = self.support.expand_as(pns) * pns
             argmax_indices_ns = dns.sum(2).argmax(1)
             self.target_net.reset_noise()
@@ -114,9 +149,9 @@ class Agent:
         self.online_net.zero_grad()
         (weights * loss).mean().backward()
         clip_grad_norm_(self.online_net.parameters(), self.norm_clip)
-        self.optimizer.step()
+        self.optimizer_o.step()
         mem.update_priorities(idxs, loss.detach().cpu().numpy())
 
-    def learn(self, mem, pgan, train_agent):
+    def learn(self, mem):
         idxs, states, actions, returns, next_states, nonterminals, weights = mem.sample(self.batch_size)
-        self._learn(mem, pgan, train_agent, idxs, states, actions, returns, next_states, nonterminals, weights)
+        self._learn(mem, idxs, states, actions, returns, next_states, nonterminals, weights)
--- network.py
+++ network.py
@@ -1,9 +1,13 @@
-# adapted from https://github.com/Kaixhin/Rainbow
+# adapted from https://github.com/Kaixhin/Rainbow and https://github.com/facebookresearch/pytorch_GAN_zoo
 
 import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from pytorch_GAN_zoo.models.networks.custom_layers import EqualizedConv2d, EqualizedLinear, NormalizationLayer, \
+    Upscale2d
+from pytorch_GAN_zoo.models.utils.utils import num_flat_features
+from pytorch_GAN_zoo.models.networks.mini_batch_stddev_module import miniBatchStdDev
 
 
 class NoisyLinear(nn.Module):
@@ -90,3 +94,190 @@ class DQN(nn.Module):
         self.fc_z_a.reset_noise()
 
 
+class GeneratorDQN(DQN):
+    def __init__(self, atoms, action_size, history_length, hidden_size, noisy_std):
+        super(GeneratorDQN, self).__init__(atoms, action_size, history_length, hidden_size, noisy_std)
+        self.depth_scale0 = 128
+        self.equalized_lr = True
+        self.init_bias_to_zero = True
+        self.dim_output = 1
+        self.dim_latent = 4096
+        self.scales_depth = [self.depth_scale0]
+
+        self.scale_layers = nn.ModuleList()
+
+        self.to_rgb_layers = nn.ModuleList()
+        self.to_rgb_layers.append(EqualizedConv2d(self.depth_scale0, self.dim_output, 1, equalized=self.equalized_lr,
+                                                  initBiasToZero=self.init_bias_to_zero))
+
+        self.format_layer = EqualizedLinear(self.dim_latent, 25 * self.scales_depth[0], equalized=self.equalized_lr,
+                                            initBiasToZero=self.init_bias_to_zero)
+
+        self.group_scale0 = nn.ModuleList()
+        self.group_scale0.append(
+            EqualizedConv2d(self.depth_scale0, self.depth_scale0, 3, padding=1, equalized=self.equalized_lr,
+                            initBiasToZero=self.init_bias_to_zero))
+
+        self.alpha = 0
+
+        self.leaky_relu = torch.nn.LeakyReLU(0.2)
+
+        self.normalization_layer = NormalizationLayer()
+
+        self.generation_activation = None
+
+    def add_scale(self, depth_new_scale):
+        depth_last_scale = self.scales_depth[-1]
+        self.scales_depth.append(depth_new_scale)
+
+        self.scale_layers.append(nn.ModuleList())
+        self.scale_layers[-1].append(
+            EqualizedConv2d(depth_last_scale, depth_new_scale, 3, padding=1, equalized=self.equalized_lr,
+                            initBiasToZero=self.init_bias_to_zero))
+        self.scale_layers[-1].append(
+            EqualizedConv2d(depth_new_scale, depth_new_scale, 3, padding=1, equalized=self.equalized_lr,
+                            initBiasToZero=self.init_bias_to_zero))
+
+        self.to_rgb_layers.append(EqualizedConv2d(depth_new_scale, self.dim_output, 1, equalized=self.equalized_lr,
+                                                  initBiasToZero=self.init_bias_to_zero))
+
+    def set_alpha(self, alpha):
+        self.alpha = alpha
+
+    def forward(self, x, skip_gan=False, use_log_softmax=False):
+        x = self.net(x)
+        x = x.view(-1, self.feat_size)
+
+        v = self.fc_z_v(F.relu(self.fc_h_v(x)))
+        a = self.fc_z_a(F.relu(self.fc_h_a(x)))
+        v, a = v.view(-1, 1, self.atoms), a.view(-1, self.action_size, self.atoms)
+        q = v + a - a.mean(1, keepdim=True)
+        q = F.log_softmax(q, dim=2) if use_log_softmax else F.softmax(q, dim=2)
+
+        if skip_gan:
+            return q, x
+
+        x = self.normalization_layer(x)
+        x = x.view(-1, num_flat_features(x))
+        x = self.leaky_relu(self.format_layer(x))
+        x = x.view(x.size()[0], -1, 3, 3)
+        x = self.normalization_layer(x)
+
+        for conv_layer in self.group_scale0:
+            x = self.leaky_relu(conv_layer(x))
+            x = self.normalization_layer(x)
+
+        if self.alpha > 0 and len(self.scale_layers) == 1:
+            y = self.to_rgb_layers[-2](x)
+            y = Upscale2d(y)
+
+        for scale, layer_group in enumerate(self.scale_layers, 0):
+            x = Upscale2d(x)
+            for conv_layer in layer_group:
+                x = self.leaky_relu(conv_layer(x))
+                x = self.normalization_layer(x)
+            if self.alpha > 0 and scale == (len(self.scale_layers) - 2):
+                y = self.to_rgb_layers[-2](x)
+                y = Upscale2d(y)
+
+        x = self.to_rgb_layers[-1](x)
+
+        if self.alpha > 0:
+            x = self.alpha * y + (1.0 - self.alpha) * x
+
+        if self.generation_activation is not None:
+            x = self.generation_activation(x)
+
+        return q, x
+
+
+class PGANDiscriminator(nn.Module):
+    def __init__(self, history_length):
+        super(PGANDiscriminator, self).__init__()
+        self.depth_scale0 = 128
+        self.equalized_lr = True
+        self.init_bias_to_zero = True
+        self.dim_input = history_length + 1
+        self.size_decision_layer = 1
+        self.mini_batch_normalization = True
+        self.dim_entry_scale0 = self.depth_scale0 + 1
+        self.scales_depth = [self.depth_scale0]
+
+        self.scale_layers = nn.ModuleList()
+
+        self.from_rgb_layers = nn.ModuleList()
+        self.from_rgb_layers.append(EqualizedConv2d(self.dim_input, self.depth_scale0, 1, equalized=self.equalized_lr,
+                                                    initBiasToZero=self.init_bias_to_zero))
+
+        self.merge_layers = nn.ModuleList()
+
+        self.decision_layer = EqualizedLinear(self.scales_depth[0], self.size_decision_layer,
+                                              equalized=self.equalized_lr, initBiasToZero=self.init_bias_to_zero)
+
+        self.group_scale0 = nn.ModuleList()
+        self.group_scale0.append(
+            EqualizedConv2d(self.dim_entry_scale0, self.depth_scale0, 3, padding=1, equalized=self.equalized_lr,
+                            initBiasToZero=self.init_bias_to_zero))
+        self.group_scale0.append(EqualizedLinear(self.depth_scale0 * 25, self.depth_scale0, equalized=self.equalized_lr,
+                                                 initBiasToZero=self.init_bias_to_zero))
+
+        self.alpha = 0
+
+        self.leaky_relu = torch.nn.LeakyReLU(0.2)
+
+    def add_scale(self, depth_new_scale):
+        depth_last_scale = self.scales_depth[-1]
+        self.scales_depth.append(depth_new_scale)
+
+        self.scale_layers.append(nn.ModuleList())
+        self.scale_layers[-1].append(
+            EqualizedConv2d(depth_new_scale, depth_new_scale, 3, padding=1, equalized=self.equalized_lr,
+                            initBiasToZero=self.init_bias_to_zero))
+        self.scale_layers[-1].append(
+            EqualizedConv2d(depth_new_scale, depth_last_scale, 3, padding=1, equalized=self.equalized_lr,
+                            initBiasToZero=self.init_bias_to_zero))
+
+        self.from_rgb_layers.append(EqualizedConv2d(self.dim_input, depth_new_scale, 1, equalized=self.equalized_lr,
+                                                    initBiasToZero=self.init_bias_to_zero))
+
+    def set_alpha(self, alpha):
+        self.alpha = alpha
+
+    def forward(self, x, get_feature=False):
+        if self.alpha > 0 and len(self.from_rgb_layers) > 1:
+            y = F.avg_pool2d(x, (2, 2))
+            y = self.leaky_relu(self.from_rgb_layers[- 2](y))
+
+        x = self.leaky_relu(self.from_rgb_layers[-1](x))
+
+        merge_layer = self.alpha > 0 and len(self.scale_layers) > 1
+
+        shift = len(self.from_rgb_layers) - 2
+
+        for group_layer in reversed(self.scale_layers):
+            for layer in group_layer:
+                x = self.leaky_relu(layer(x))
+
+            x = nn.AvgPool2d((2, 2))(x)
+
+            if merge_layer:
+                merge_layer = False
+                x = self.alpha * y + (1 - self.alpha) * x
+
+            shift -= 1
+
+        if self.mini_batch_normalization:
+            x = miniBatchStdDev(x)
+
+        x = self.leaky_relu(self.group_scale0[0](x))
+
+        x = x.view(-1, num_flat_features(x))
+
+        x = self.leaky_relu(self.group_scale0[1](x))
+
+        out = self.decision_layer(x)
+
+        if not get_feature:
+            return out
+
+        return out, x
