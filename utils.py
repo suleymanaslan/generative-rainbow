572c57908cb662e9dc641ba3768d9ca917e55941
suleymanaslan <suleymanaslan15@gmail.com>
2020-09-16 13:25:55 +0300

    Evaluation

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 class Trainer:
     def __init__(self, episodes, replay_frequency, reward_clip, max_steps, learning_start_step,
-                 target_update, gan_steps):
+                 target_update, gan_steps, eval_steps):
         self.episodes = episodes
         self.replay_frequency = replay_frequency
         self.reward_clip = reward_clip
@@ -17,8 +17,11 @@ class Trainer:
         self.learning_start_step = learning_start_step
         self.target_update = target_update
         self.gan_steps = gan_steps
+        self.eval_steps = eval_steps
         self.ep_rewards = []
         self.ep_steps = []
+        self.eval_ep_rewards = []
+        self.eval_ep_steps = []
         self.avg_ep_rewards = None
         self.model_dir = None
 
@@ -26,7 +29,7 @@ class Trainer:
         print(text)
         print(text, file=open(f'{self.model_dir}/log.txt', 'a'))
 
-    def train(self, env, agent, mem, notebook_file=None):
+    def train(self, env, test_env, agent, mem, notebook_file=None):
         training_timestamp = str(int(time.time()))
         self.model_dir = f'trained_models/model_{training_timestamp}/'
 
@@ -53,6 +56,8 @@ class Trainer:
                 if steps >= self.max_steps:
                     finished = True
                     break
+                if steps % self.eval_steps == 0:
+                    self.eval(test_env, agent, steps)
                 if self.reward_clip > 0:
                     reward = max(min(reward, self.reward_clip), -self.reward_clip) / self.reward_clip
                 mem.append(observation, action, reward, done)
@@ -71,7 +76,7 @@ class Trainer:
                 self.plot()
             if episode_ix == 1 or episode_ix % 10 == 0:
                 self.print_and_log(f"{datetime.now()}, episode:{episode_ix:5d}, step:{steps:6d}, "
-                                   f"reward:{ep_reward:2.0f}"),
+                                   f"reward:{ep_reward:4.1f}"),
             if finished:
                 break
         self.print_and_log(f"{datetime.now()}, end training")
@@ -79,7 +84,7 @@ class Trainer:
     def plot(self, close=True):
         self.avg_ep_rewards = [np.array(self.ep_rewards[max(0, i - 150):max(1, i)]).mean()
                                for i in range(len(self.ep_rewards))]
-        max_reward = 20
+        max_reward = 10
         plt.style.use('default')
         sns.set()
         plt.figure(figsize=(10, 6))
@@ -89,6 +94,8 @@ class Trainer:
         plt.xticks(np.arange(0, self.max_steps + 1, self.max_steps // 6))
         plt.plot(self.ep_steps, self.ep_rewards, alpha=0.5)
         plt.plot(self.ep_steps, self.avg_ep_rewards, linewidth=3)
+        if len(self.eval_ep_rewards) > 0:
+            plt.plot(self.eval_ep_steps, self.eval_ep_rewards, linewidth=3)
         plt.xlabel('steps')
         plt.ylabel('episode reward')
         plt.savefig(f"{self.model_dir}/training.png")
@@ -100,19 +107,24 @@ class Trainer:
 
         np.save(f"{self.model_dir}/ep_rewards.npy", self.ep_rewards)
         np.save(f"{self.model_dir}/ep_steps.npy", self.ep_steps)
+        np.save(f"{self.model_dir}/eval_ep_rewards.npy", self.eval_ep_rewards)
+        np.save(f"{self.model_dir}/eval_ep_steps.npy", self.eval_ep_steps)
 
         self.plot(close=False)
         plt.show()
 
-    def eval(self, env, agent):
-        agent.eval()
-        for _ in range(10):
+    def eval(self, env, agent, steps):
+        eval_reward = 0
+        for _ in range(env.num_levels):
             observation, ep_reward, ep_step, done = env.reset(), 0, 0, False
-            env.render()
             while not done:
-                action = agent.act_e_greedy(observation)
-                next_observation, reward, done, info = env.step(action, render=True)
+                action, _ = agent.act(observation)
+                next_observation, reward, done, info = env.step(action)
                 ep_reward += reward
                 ep_step += 1
                 observation = next_observation
-            self.print_and_log(f"{datetime.now()}, eval ep_step:{ep_step:5d}, reward:{ep_reward:10.4f}")
+            eval_reward += ep_reward
+        eval_reward /= env.num_levels
+        self.eval_ep_rewards.append(eval_reward)
+        self.eval_ep_steps.append(steps)
+        self.print_and_log(f"{datetime.now()}, eval reward:{eval_reward:4.1f}")
